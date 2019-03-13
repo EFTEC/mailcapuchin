bc5f34f1dc3fbf8c567b306bc96e97a6a528d77b
Jorge Castro <jorge.castro.c@gmail.com>
2019-03-13 19:33:51 -0300

    0.1

---
+++ lib/MailCapuchin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace eftec\MailCapuchin;
+
+
+class MailCapuchin
+{
+
+	/**
+	 * MailCapuchin constructor.
+	 */
+	public function __construct()
+	{
+	}
+	
+	public function addProject() {
+		
+	}
+	public function runProject($rerun=false) {
+		
+	}
+	public function genPixel() {
+		
+	}
+	public function showUI() {
+		
+	}
+	
+}
